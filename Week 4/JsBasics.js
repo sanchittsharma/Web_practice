80c80e27153da92e28b514f2788b9ba3d4d36494
sanchittsharma <142312010+sanchittsharma@users.noreply.github.com>
2025-03-24 20:08:38 +0530

    Add files via upload

---
+++ Week 4/JsBasics.js
@@ -0,0 +1,50 @@
+console.log("JavaScript Basics");
+
+// comment single line 
+/*
+multiline comment 
+in js */
+
+// variables in js 
+// let - block scoped 
+let a=5;
+// var - globally scoped 
+var b=6;
+
+const c=10;
+
+// data types 
+// int and float 
+var n=10;
+var m=10.1;
+// string 
+let str='String in JS';
+let str2="can be in single or double quotes";
+
+// array 
+
+let arr=[1,"array","new" ,2];
+console.log(arr);
+
+// operators in js 
+
+// Arithmetic Operators 
+  var sum=5+10;
+  var mul=5*2;
+  console.log(sum);
+
+//   assignment 
+var e=4;
+// comparison operator 
+   console.log(a==b);
+
+//    logical operators --> && || ! 
+// && return true if both operands are true 
+  console.log(a>b && sum<mul) 
+
+//   || return true if one operand id true 
+console.log(a==b || b>a)
+// ! retrun true if operand is false 
+
+
+
